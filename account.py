cd1bc2d9f892d90cd253df27d6f93a9ef1c624c5
Asia <asiasharif12@icloud.com>
2021-03-16 13:08:42 +0000

    updated homework felicia

--- account.py
+++ account.py
@@ -29,14 +29,40 @@ class Account:
         return f"Your current balance is £{self.balance}"
 
     def withdraw(self, amount):
-        #self.balance -= amount
-        #return f"You have successfully withdrawn £{amount}, your new balance is {self.balance}"
-        if (self.balance - amount) < amount:
-            raise InsufficientFundsException(f"You have insufficient funds in your account to withdraw £{amount}\n" + f"Your current balance is £{self.balance}, you will be overdrawn by £ {abs(self.balance - amount)}.Please check you have funds available in your account before processing this transaction")
+        if (self.balance - amount) < 0:
+            print("declined")
+            raise InsufficientFundsException("could not complete transaction, insufficient funds") #the err argument came from here
+        #insufficient funds is a class - when we raise it - we are creating the object - what ever we put in the bracket is going to be an arugment of that object
+        #class throws error in the air and client script is going to catch the error thats why it is ebing raised here
+        #try except is being implemented in the client script
+        else:
+            print("not an error")
+            return self.balance
+
+
+
+        # try:
+        #     self.balance -= amount
+        # except InsufficientFundsException:
+        #     print(f"You have insufficient funds in your account to withdraw £{amount}\n" + f"Your current balance is £{self.balance}, you will be overdrawn by £ {abs(self.balance - amount)}.Please check you have funds available in your account before processing this transaction")
+        #
+        # else:
+        #     return f"You have successfully withdrawn £{amount}, your new balance is {self.balance}"
+
+
+            # return f"You have successfully withdrawn £{amount}, your new balance is {self.balance}"
+
+        # if (self.balance - amount) < amount:
+            # raise InsufficientFundsException(f"You have insufficient funds in your account to withdraw £{amount}\n" + f"Your current balance is £{self.balance}, you will be overdrawn by £ {abs(self.balance - amount)}.Please check you have funds available in your account before processing this transaction")
 
     def get_account_details(self):
         return f"The Account for {self.fullname} was opened at {self.branch}\n" + f"Account Number: {self.account_no}\n" + f"Sort Code: {self.sort_code}\n" + f"The Balance today is £{self.balance}"
-
-ac1 = Account("Faridah Dada","40 04 15", "7567860", "Westfield Stratford", 500)
-print(ac1.get_account_details())
+#
+# ac1 = Account("Faridah Dada","40 04 15", "7567860", "Westfield Stratford", 500)
+# print(ac1.withdraw(600))
+# (a,b) = (6,0)
+# try:# simple use of try-except block for handling errors
+#     g = a/b
+# except ZeroDivisionError:
+#     print ("This is a DIVIDED BY ZERO error")
 
--- client_script.py
+++ client_script.py
@@ -1,19 +1,28 @@
 from account_errors import InsufficientFundsException
-from customer import Customer
-from savings_account import Savings_account
-from current_account import Current_account
+# # from customer import Customer
+# from savings_account import Savings_account
+# from current_account import Current_account
+from account import Account
 
-jessie_savings = Savings_account(500, 2.5, 3, 2)
-jessie_savings.withdraw(300)
-print(jessie_savings.get_balance())
-jessie_savings.withdraw(500)
+# jessie_savings = Savings_account(500, 2.5, 3, 2)
+# jessie_savings.withdraw(300)
+# print(jessie_savings.get_balance())
+# jessie_savings.withdraw(500)
+#
+# asia_current = Current_account(1000, -500, 500)
+# print(asia_current.get_balance())
+# print(asia_current.send_money(100, "jessie"))
+# print(asia_current.get_balance())
+# asia_current.send_money(3000, "jackie")
+# asia_current.receive_money(50, "jessie")
+# print(asia_current.get_balance())
 
-asia_current = Current_account(1000, -500, 500)
-print(asia_current.get_balance())
-print(asia_current.send_money(100, "jessie"))
-print(asia_current.get_balance())
-asia_current.send_money(3000, "jackie")
-asia_current.receive_money(50, "jessie")
-print(asia_current.get_balance())
+victoria = Account('Asia Sharif', 601320, 45649324, 'Natwest', 500)
+try:
+    victoria.withdraw(600)
+except InsufficientFundsException as err: #err is a local variable to the except - any arguments in insufficent funds will be assigned to err
+    print(err)
+else:
+    print("successful transaction")
 
 
