ef98feb4f552907f352ca350fea2e938061fa2de
Faridah <faridahdada@outlook.com>
2021-03-16 10:59:02 +0000

    added in exception and making changes to account class

--- account.py
+++ account.py
@@ -1,16 +1,17 @@
 import random
+from account_errors import InsufficientFundsException
 class Account:
     no_created = 0
-    def __init__(self, fullname, balance):
+    def __init__(self, fullname,sort_code, account_no, branch, balance):
         self.fullname = fullname
-        self.sort_code = "40 04 15"
-        self.account_no = random.randint(100000, 999999)
-        self.branch = "Westfield Stratford Branch"
+        self.sort_code = sort_code
+        self.account_no = account_no
+        self.branch = branch
         self.balance = balance
         Account.no_created +=1
 
-    def __str__(self):
-        return f"The Account for {self.fullname}was opened at {self.branch}\n." + f"Account Number: {self.account_no}\n"  + f"Sort Code: {self.sort_code}\n" + f" The Balance today is {self.balance}"
+    #def __str__(self):
+        #return f"The Account for {self.fullname} was opened at {self.branch}\n." + f"Account Number: {self.account_no}\n"  + f"Sort Code: {self.sort_code}\n" + f" The Balance today is {self.balance}"
 
     def get_branch_location(self):
         return self.branch
@@ -24,12 +25,18 @@ class Account:
     def deposit(self, amount):
         self.balance += amount
 
-    def withdraw(self, amount):
-        self.balance -= amount
-        return f"You have successfully withdrawn £{amount}, your new balance is {self.balance}"
-
     def get_balance(self):
         return f"Your current balance is £{self.balance}"
 
+    def withdraw(self, amount):
+        #self.balance -= amount
+        #return f"You have successfully withdrawn £{amount}, your new balance is {self.balance}"
+        if (self.balance - amount) < amount:
+            raise InsufficientFundsException(f"You have insufficient funds in your account to withdraw £{amount}\n" + f"Your current balance is £{self.balance}, you will be overdrawn by £ {abs(self.balance - amount)}.Please check you have funds available in your account before processing this transaction")
 
+    def get_account_details(self):
+        return f"The Account for {self.fullname} was opened at {self.branch}\n" + f"Account Number: {self.account_no}\n" + f"Sort Code: {self.sort_code}\n" + f"The Balance today is £{self.balance}"
+
+ac1 = Account("Faridah Dada","40 04 15", "7567860", "Westfield Stratford", 500)
+print(ac1.get_account_details())
 
--- current_account.py
+++ current_account.py
@@ -1,19 +1,20 @@
+import random
 from account_errors import InsufficientFundsException
 from account import Account
 class Current_account(Account):
-    def __init__(self,fullname, balance, overdraft,spend_limit):
-        Account.__init__(self,fullname, balance)
+    def __init__(self,fullname, sort_code,  account_no, branch , balance, overdraft,spend_limit):
+        super().__init__(fullname,sort_code,account_no, branch,balance)
         self._overdraft = overdraft
         self._spend_limit = spend_limit
 
-    def __str__(self):
-        super().__str__()
-        return f"Overdraft: {self._overdraft}\n" + f"Spending Limit: {self._spend_limit}"
+    def get_dets(self):
+        st = super().__init__(fullname, sort_code, account_no, branch , balance, overdraft,spend_limit)
+        return st + f"Overdraft: {self._overdraft}\n" + f"Spending Limit: {self._spend_limit}"
 
     def send_money(self, amount, recipient):
         if amount < self._spend_limit:
             self.balance -= amount
-            return f"you have sent £{amount} to {recipient}"
+            return f'you have sent £{amount} to {recipient}'
         else:
             return f"Could not send £{amount} to {recipient}, as it is larger than your spending limit of £{self._spend_limit}"
 
@@ -25,5 +26,5 @@ class Current_account(Account):
         if self.balance - amount <= self._overdraft:
             raise InsufficientFundsException(f"You have insufficient funds in your account to make the withdraw of £{amount}. Please Check you have the funds available in your account before making this withdraw")
 
-ac1 = Current_account("Faridah Dada", 50, 20,100)
-print(ac1.make_withdrawal(500))
+ac1 = Current_account("Faridah Dada","40 04 15", "7567860", "Westfield Stratford", 5, 10, 10)
+print(ac1.get_dets())
